6a6a334813578eeb70ba632cbd2d03f2f73a118a
Yehchenghan <113499336+Yehchenghan@users.noreply.github.com>
2024-11-06 19:21:50 +0800

    Update AI專題 - test.py
    改金鑰

--- AI專題 - test.py
+++ AI專題 - test.py
@@ -2,7 +2,7 @@
 import requests
 
 # 你的 40 Mini API 金鑰
-api_key = "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"
+api_key = "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"
 api_url = "https://ncuedu.tw/mygpts/2200/4408"
 headers = {
     "Authorization": f"Bearer {api_key}",
